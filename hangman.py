422591863af920df5b5294b94c8dbf33c1af127f
suprrraa <156365820+suprrraa@users.noreply.github.com>
2024-03-20 19:06:24 +0000

    adding correct guesses to guessed word

--- hangman.py
+++ hangman.py
@@ -2,13 +2,8 @@
 # Create your own ASCII art if you desire, but
 # ONLY AFTER getting the game logic working.
 from ascii_art import BANNER, HANGMAN_PICS
-
-# uncomment the import statement, below, when
-# you're ready to implement a one player version
-# of the game.
-# `animal_words` is a list of . . . animal words.
-# Feel free to add more words or word categories.
-# from word_lists import animal_words
+from random import choice
+from word_lists import animal_words
 
 """
 Here's where you'll write your code. 
@@ -35,10 +30,30 @@ Tests? No tests for this project.
 # that will orchestrate all the helper functions
 # you define, above.
 def play_hangman():
+
+    print("Welcome To Hangman")
+    x=input("press enter to start")
+    secret_word = choice(animal_words)
+    #print(secret_word)(use at end of game)
+    guessed_word = ['_'] * len(secret_word)
+    while '_' in guessed_word:
+        print(' '.join(guessed_word))
+        guess = input("guess a letter: ")
+        # print("you guessed: ", guess)
+        for index, letter in enumerate(secret_word):
+            if guess == letter:
+                guessed_word[index] = guess 
+                if guess = 7 
+
+        print(guessed_word)
+      
+
+      
+
+def secret_word():
     pass
 
-
-"""
+""" 
 Don't worry about the code below, and don't change it.
 
 It's just a way to trigger the `play_hangman` function
